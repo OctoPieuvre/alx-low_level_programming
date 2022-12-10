f55c6baa5c1b1fe772c89a483ff56e0a4638404e
OctoPieuvre <dieudonnezin@outlook.com>
2022-12-10 01:22:14 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print base 10 digits
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int d;
+
+
+	for (d = 48; d <= 57; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
