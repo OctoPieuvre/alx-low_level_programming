946d8c502ab464efae4dacc37519734f59fb10b1
OctoPieuvre <dieudonnezin@outlook.com>
2022-12-10 00:03:35 -0800

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	char c = a;
+	while (c <= z)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(n);
+	return (0);
+}
