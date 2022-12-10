6f2ad29711087fa16e29caf85126b9e7bb48b591
OctoPieuvre <dieudonnezin@outlook.com>
2022-12-10 06:51:37 -0800

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *
+ * main - print double digits
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i, e, g, h, op1, op2;
-	i = e = g = h = 48;
 
+	i = e = g = h = 48;
 	while (h < 58)
 	{
 		g = 48;
