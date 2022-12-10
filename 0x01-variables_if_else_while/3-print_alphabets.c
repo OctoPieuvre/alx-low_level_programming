6e35ed64f2be3ff6baa909e68ec5ff07e2978eae
OctoPieuvre <dieudonnezin@outlook.com>
2022-12-10 00:41:33 -0800

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	char c[26] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(c[i]);
+	}
+	putchar('\n');
+	return (0);
+}
