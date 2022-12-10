5e1c1021e8b2572971c629d889aa4dfd65a6ba0c
OctoPieuvre <dieudonnezin@outlook.com>
2022-12-10 01:05:30 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print base 10 digits
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d <= 10; d++)
+	{
+		printf(d);
+	}
+	printf('\n');
+	return (0);
+}
