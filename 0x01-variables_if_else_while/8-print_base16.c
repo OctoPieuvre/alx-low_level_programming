7c19bd72d89d5610d7a2c93b9877c5ff9d50fd3e
OctoPieuvre <dieudonnezin@outlook.com>
2022-12-10 01:42:11 -0800

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print base 16 digits
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int d;
+	char c;
+
+	for (d = 0; d < 10; d++)
+	{
+		putchar(d);
+	}
+	for (c = 'f'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
