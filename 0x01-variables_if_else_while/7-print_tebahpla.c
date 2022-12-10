9d7cdbbc297314ffa9b1d012da5bf77057e328f4
OctoPieuvre <dieudonnezin@outlook.com>
2022-12-10 01:29:32 -0800

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints in reversed alphabet
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
