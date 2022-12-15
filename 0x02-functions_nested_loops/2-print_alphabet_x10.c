abf75a45e5d8f530eeafb640a024bdceba652d13
OctoPieuvre <dieudonnezin@outlook.com>
2022-12-15 14:56:00 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times alphabet
+ *
+ * Return: Alwways 0
+ */
+void print_alphabet_x10(void)
+{
+	char s = 'a';
+	int d = 0;
+
+	for (d = 0, d < 10, d++)
+	{
+		while (s <= 'z')
+		{
+			_putchar(s);
+			s++;
+		}
+		_putchar('\n');
+	}
+}
