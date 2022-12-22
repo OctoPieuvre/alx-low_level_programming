392eaba8b7cf718fe9cd9ded87cf1ff4e052dfe6
OctoPieuvre <dieudonnezin@outlook.com>
2022-12-22 14:45:14 -0800

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * times_table - print 9 times table
+ *
+ * Return: Noting
+ */
+void times_table(void)
+{
+	int c, d, e;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar('0');
+		for (d = 0; d < 10; d++)
+		{
+			e = c * d;
+			_putchar(',');
+			_putchar(' ');
+			if (e < 10)
+			{
+				_putchar(' ');
+				_putchar(e + '0');
+			}
+			else
+			{
+				_putchar((e / 10) + '0');
+				_putchar((e % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
