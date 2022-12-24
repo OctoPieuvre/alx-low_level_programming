e7f268cf29a43af8f998f65d768c576da46c2641
OctoPieuvre <dieudonnezin@outlook.com>
2022-12-24 11:39:10 -0800

    10.a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @c: first integer
+ * @d: second interger
+ * Return: return nothing
+ */
+int add(int c, int d)
+{
+	int f = c + d;
+	return (f);
+}
