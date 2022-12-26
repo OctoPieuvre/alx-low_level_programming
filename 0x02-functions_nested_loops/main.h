a9121c6e1f9bd99a5760d820078cd62ae7814629
OctoPieuvre <dieudonnezin@outlook.com>
2022-12-26 13:35:25 -0800

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
+
+
+
 #define MAIN_H
-/**
+/*
  * File: main.h
- * Desc : header file
+ *
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
  */
 int _putchar(char c);
 void print_alphabet(void);
@@ -16,5 +21,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_tables(int n);
+void print_times_table(int n);
 #endif
