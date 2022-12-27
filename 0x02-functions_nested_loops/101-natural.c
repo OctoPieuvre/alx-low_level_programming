2f513fa4f2f3e2aa7634d69e16eddb6c3299fd99
OctoPieuvre <dieudonnezin@outlook.com>
2022-12-26 21:49:57 -0800

    13. Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints natural nimbers below 1024
+ * 
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a  = 1; a <= 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+		{
+			b += a;
+		}
+	printf("%d\n", b);
+	return (0);
+}
