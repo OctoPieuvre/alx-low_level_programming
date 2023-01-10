81acb97f30231fadb2d64c78d72e3729a296c16b
OctoPieuvre <dieudonnezin@outlook.com>
2023-01-10 05:24:44 -0800

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,15 @@ int main(void)
 
 	for (n = 1; n < 101; n++)
 	{
-		if ((n % 3) == 0)
+		if (n % 3 == 0)
 		{
 			printf(" Fizz");
 		}
-		else if ((n % 5) == 0)
+		else if (n % 5 == 0)
 		{
 			printf(" Buzz");
 		}
-		else if ((n % 3) == 0 && (n % 5) == 0)
+		else if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf(" FizzBuzz");
 		}
