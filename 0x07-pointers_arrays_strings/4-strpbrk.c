e3bf8f968aa37a7a079e4cad4fe65439f25316f5
OctoPieuvre <Dieudonnezin@outlook.com>
2023-03-21 20:42:26 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strpbrk - function that searching a string
+ * @s: string
+ * @accept: checker
+ * Return: pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
