e059440f9718d4798f3d23db54819e6191a413b7
OctoPieuvre <Dieudonnezin@outlook.com>
2023-03-22 14:40:02 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard - write a function that print the chessboard
+ * @a: input
+ * Return: 0
+ */
+
+void print_chessboqrd(char (*a)[8])
+{
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			printf("%c ", a[i][j]);
+		}
+		printf("\n");
+	}
+	return (0);
+}
